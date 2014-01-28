854b9a22f37536b7de2fbf098b9e00c2654354ca
redstrato <kkygang@gmail.com>
2014-01-28 16:27:08 +0900

    Refactoring

--- src/main/java/io/resthelpersample/model/Hotel.java
+++ src/main/java/io/resthelpersample/model/Hotel.java
@@ -1,10 +1,18 @@
 /*
- * @(#)Hotel.java $version 2014. 1. 24.
+ * Copyright 2014 The RestHelper Project
  *
- * Copyright 2007 NHN Corp. All rights Reserved. 
- * NHN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * The RestHelper Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
  */
-
 package io.resthelpersample.model;
 
 import java.util.ArrayList;
@@ -13,7 +21,7 @@ import java.util.List;
 import org.codehaus.jackson.annotate.JsonIgnore;
 
 /**
- * @author NHN
+ * @author redstrato
  */
 public class Hotel {
 	private int number;
